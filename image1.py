19b10554fe8c721951337a947eb0f95b0efd8728
Charlene Lo <lo861119@gmail.com>
2022-08-22 15:45:19 +0800

    os.path.convert()

--- image1.py
+++ image1.py
@@ -3,7 +3,7 @@ import os
 
 for file in os.listdir('orig'):  #for all the documents in the file
 	if file.endswith('.jpg'):
-		image_file = Image.open('orig/' + file)
+		image_file = Image.open(os.path.join('orig', file))
 		image_file = image_file.convert('L')
-		image_file.save('result/' + file[:-4] + '_grey.png')
+		image_file.save(os.path.join('result/',file[:-4] + '_grey.png'))
 
